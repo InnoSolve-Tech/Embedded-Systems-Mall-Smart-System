e5bf2d9d41fda9ec05ef98a2fedef653c713c5fe
BenziDarwin <ssalibenjamin0402@gmail.com>
2024-10-16 08:59:51 +0300

    Initial Commit

---
+++ main.c
@@ -0,0 +1,18 @@
+/*
+ * Kasamba Smart Mall System.c
+ *
+ * Created: 10/16/2024 8:58:59 AM
+ * Author : ssali
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
